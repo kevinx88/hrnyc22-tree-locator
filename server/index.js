19ceb47a61dcf4797557254c2e664358042e8486
kgxu <Kevinx88@gmail.com>
2019-06-25 21:15:59 -0400

    complete submit coordinates with local data

--- server/index.js
+++ server/index.js
@@ -45,6 +45,6 @@ app.post('/items', function(req, res) {
   });
 });
 
-app.listen(3000, function() {
-  console.log('listening on port 3000!');
+app.listen(3011, function() {
+  console.log('listening on port 3011!');
 });
